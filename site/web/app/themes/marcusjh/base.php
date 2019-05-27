64685c32dfb7214ed07653405cb381d55f530886
marcusharding <marcus.harding@mrbandfriends.co.uk>
2019-05-27 16:13:46 +0100

    Still not sused wrapper but started tidying and breaking up index

--- site/web/app/themes/marcusjh/base.php
+++ site/web/app/themes/marcusjh/base.php
@@ -1,22 +1,25 @@
+<?php 
+ use Roots\Sage\Wrapper;
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
     <!-- Pulling in head content -->
-    <?php get_template_part('head'); ?>
+    <?php get_template_part('templates/head'); ?>
     <!-- BODY CONTENT -->
     <body id="page-top" <?php body_class();?>>
         <!-- PAGE CONTENT -->
-        <div>
             <!-- Grab the header -->
             <?php
-                do_action('get_header');
-                get_template_part('templates/header');
+					do_action('get_header');
+					get_template_part('templates/header');
             ?>
-
+            
             <main>
-                <div data-route>
+                <body id="page-top">
                     <?php include Wrapper\template_path(); ?>
-                </div>
+                </body>
             </main>
 
             <p id="back-top">
@@ -24,9 +27,8 @@
             </p>
 
             <?php
-                do_action('get_footer');
-                get_template_part('templates/footer');
-            ?>
-        </div>
-    </body>
+					do_action('get_footer');
+					get_template_part('templates/footer');
+			?>
+            </body>
 </html>
---
+++ site/web/app/themes/marcusjh/lib/wrapper.php
@@ -0,0 +1,34 @@
+<?php
+namespace Roots\Sage\Wrapper;
+?>
+
+<?php
+# License: Public Domain
+# I recommend replacing 'my_' with your own prefix.
+function template_path() {
+	return My_Wrapping::$main_template;
+}
+function template_base() {
+	return My_Wrapping::$base;
+}
+class My_Wrapping {
+	/**
+	 * Stores the full path to the main template file
+	 */
+	static $main_template;
+	/**
+	 * Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+	 */
+	static $base;
+	static function wrap( $template ) {
+		self::$main_template = $template;
+		self::$base = substr( basename( self::$main_template ), 0, -4 );
+		if ( 'index' == self::$base )
+			self::$base = false;
+		$templates = array( 'base.php' );
+		if ( self::$base )
+			array_unshift( $templates, sprintf( '-%s.php', self::$base ) );
+		return locate_template( $templates );
+	}
+}
+add_filter( 'template_include', array( 'My_Wrapping', 'wrap' ), 99 );
