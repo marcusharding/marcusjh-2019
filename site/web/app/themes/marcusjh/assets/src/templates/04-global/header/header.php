6722c0f2a13f145d90b0bca548c7c2b759493a29
marcusharding <marcus.harding01@googlemail.com>
2019-08-27 18:22:22 +0100

    slight nav fix | file changes | trellis deploy success

--- site/web/app/themes/marcusjh/assets/src/templates/04-global/header/header.php
+++ site/web/app/themes/marcusjh/assets/src/templates/04-global/header/header.php
@@ -7,7 +7,7 @@ HEADER
 		<!-- Include logo -->
 		<?php include $_SERVER["DOCUMENT_ROOT"] . '/app/themes/marcusjh/assets/src/templates/01-objects/logo/logo.php'; ?>
 		<!-- Include nav -->
-		<?php include $_SERVER["DOCUMENT_ROOT"] . '/app/themes/marcusjh/assets/src/templates/03-global/nav/nav.php'; ?>
+		<?php include $_SERVER["DOCUMENT_ROOT"] . '/app/themes/marcusjh/assets/src/templates/04-global/nav/nav.php'; ?>
 		<!-- Include burger -->
 		<?php include $_SERVER["DOCUMENT_ROOT"] . '/app/themes/marcusjh/assets/src/templates/01-objects/burger/burger.php'; ?>
 	</div>
--- site/web/app/themes/marcusjh/assets/src/templates/04-global/nav/nav.php
+++ site/web/app/themes/marcusjh/assets/src/templates/04-global/nav/nav.php
@@ -2,8 +2,8 @@
 $menu = wp_get_nav_menu_items( 'primary_menu');
 ?>
 
-<nav id="siteNav" class="g-nav mobile-menu">
-	<ul class="g-nav__list justify-center align-center py-20 lg:py-0">
+<nav id="siteNav" class="g-nav mobile-menu overflow-hidden">
+	<ul class="g-nav__list justify-center align-center overflow py-20 lg:py-0">
 		<?php foreach($menu as $item): ?>
 			<li class="md:ml-4 lg:flex-auto pb-12 lg:pb-0">
 				<a class="g-nav__link text-thin" href="<?= $item->url;?>">
