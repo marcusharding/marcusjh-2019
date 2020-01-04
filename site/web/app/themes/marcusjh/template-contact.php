0adf588c1fbe61f68f7117a85e781791672f06c7
marcusharding <marcus.harding01@googlemail.com>
2020-01-04 23:56:25 +0000

    Header adjustments and contact form bug testing

--- site/web/app/themes/marcusjh/template-contact.php
+++ site/web/app/themes/marcusjh/template-contact.php
@@ -11,7 +11,7 @@ use marcusjh\lib\Utils;
 CONTACT FORM
 --------------------------->
 <section class="bg-primary h-auto lg:h-full flex justify-center items-center relative">
-    <div class="wrapper pt-24 pb-12 lg:py-24">
+    <div class="wrapper pt-24 pb-12 lg:py-24 relative">
         <p class="hidden md:block uppercase text-white text-xl text-bold pb-8">Contact me</p>
         <?= Utils\ob_load_template_part('templates/05-forms/lets-work-together/lets-work-together', [
             'shortcode' => get_field('form_shortcode')
