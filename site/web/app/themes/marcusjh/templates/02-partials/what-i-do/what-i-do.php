1457b2c989f6e68deb5acd6ff06a08119392f037
marcusharding <marcus.harding01@googlemail.com>
2020-01-22 23:59:35 +0000

    Add leading to text

--- site/web/app/themes/marcusjh/templates/02-partials/what-i-do/what-i-do.php
+++ site/web/app/themes/marcusjh/templates/02-partials/what-i-do/what-i-do.php
@@ -7,5 +7,5 @@ use marcusjh\lib\Utils;
 
 <div class="relative what-i-do">
 	<p class="text-xl lg:text-3xl text-medium w-full z-10 text-white w-2/3 home-about__heading pb-12"><?= $heading; ?></p>
-	<p class="text-white lg:w-2/3"><?= $text; ?></p>
+	<p class="text-white lg:w-2/3 leading-normal"><?= $text; ?></p>
 </div>
