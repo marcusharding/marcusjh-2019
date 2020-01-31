cd2dacdf928ab19bfd3bff603fa0fd0af19b9ea2
marcusharding <marcus.harding01@googlemail.com>
2020-01-31 19:20:14 +0000

    Update contact section height

--- site/web/app/themes/marcusjh/template-contact.php
+++ site/web/app/themes/marcusjh/template-contact.php
@@ -10,7 +10,7 @@ use marcusjh\lib\Utils;
 <!-------------------------
 CONTACT FORM
 --------------------------->
-<section class="bg-primary h-full flex justify-center items-center relative">
+<section class="bg-primary h-full lg:h-auto flex justify-center items-center relative">
     <div class="wrapper pt-24 pb-12 lg:py-24 relative">
         <p class="hidden md:block uppercase text-white text-xl text-bold pb-8">Contact me</p>
         <?= Utils\ob_load_template_part('templates/05-forms/lets-work-together/lets-work-together', [
