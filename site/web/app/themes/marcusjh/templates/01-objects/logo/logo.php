21a6bad9eadb09a30984d03df10f003828782f11
marcusharding <marcus.harding01@googlemail.com>
2020-07-01 13:44:23 +0100

    sort out featured work overlays and a few tweaks

--- site/web/app/themes/marcusjh/templates/01-objects/logo/logo.php
+++ site/web/app/themes/marcusjh/templates/01-objects/logo/logo.php
@@ -21,9 +21,3 @@ Contact: 21
 <a id="logo" class="<?= $nav_colour_modifier === null ? null : 'text-' . $nav_colour_modifier; ?> logo navbar-brand uppercase text-lg lg:text-2xl tracking-wider z-10" href="/">
 	<span class="text-extra-bold">marcusjh</span><span class="text-thin">dev</span>
 </a>
-
-<!-- <a class="w-1/5">
-	<div class="responsive-img aspect-ratio">
-		<img class="" src="/app/themes/marcusjh/assets/dist/img/me.jpg" />
-	</div>
-</a> -->
--- site/web/app/themes/marcusjh/templates/03-components/teaser/teaser-featured-work-main.php
+++ site/web/app/themes/marcusjh/templates/03-components/teaser/teaser-featured-work-main.php
@@ -10,15 +10,14 @@
 ?>
 
 <!-- DESKTOP -->
-<div data-aos="fade-up" data-aos-delay="<?= $index . '00';?>" data-index="<?= $index; ?>" class="hidden lg:flex py-8 small-project__container relative">
+<div data-aos="fade-up" data-aos-delay="<?= $index . '00';?>" data-index="<?= $index; ?>" class="hidden lg:flex py-8 small-project__container relative project__container">
   <div class="aspect-ratio h-auto project-img__container overflow-hidden rounded-t">
     <a href="<?= $postLink; ?>">
       <img alt="" draggable="false" class="absolute top-0 left-0 w-full h-full rounded-t" src="<?= $main_image; ?>"> </img>
         <div class="project-post__overlay">
             <a href="<?= $postLink; ?>" class="uppercase text-white text-xl text-bold">View Project</a>
-            <hr class="w-1/3 text-white my-4">
+            <hr class="text-white my-4">
             <div class="flex flex-wrap justify-center">
-              <!-- <p class="text-white text-md pr-2"><</p> -->
               <?php foreach($tags as $i => $tag): ?>
 
                 <?php
@@ -26,7 +25,6 @@
                 ?>
               <p class="text-white text-md pr-2 pb-2"><?= $tag->name; ?> <?= $i === $len - 1 ? null : '|' ?></p>
               <?php endforeach; ?>
-              <!-- <p class="text-white text-md pr-2">/></p> -->
             </div>
         </div>
         </a>
@@ -38,7 +36,7 @@
 </div>
 
 <!-- MOBILE -->
-<div data-aos="fade-up" data-aos-delay="<?= $index . '00';?>" data-index="<?= $index; ?>" class="wrapper lg:hidden py-8">
+<div data-aos="fade-up" data-aos-delay="<?= $index . '00';?>" data-index="<?= $index; ?>" class="lg:hidden py-8">
     <div class="aspect-ratio h-auto project-img__container overflow-hidden rounded-t">
       <a href="<?= $postLink; ?>">
           <img alt="" draggable="false" class="absolute top-0 left-0 w-full h-full rounded-t" src="<?= $main_image; ?>"> </img>
